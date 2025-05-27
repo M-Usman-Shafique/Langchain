fb34efdab6d472855c2be00e3ab261406acb010d
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-27 07:24:06 +0500

    Anthropic ChatModel

---
+++ Models/ChatModels/chatAnthropic.py
@@ -0,0 +1,10 @@
+ ## Run python3 Models/ChatModels/chatAnthropic.py
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatAnthropic(model= "claude-3-5-sonnet-20241022", temperature=0.5)
+result = model.invoke("What's the capital of France?")
+
+print(result.content)
