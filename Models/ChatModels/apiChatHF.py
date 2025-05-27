88baf8e507fe1dca4d955b30cc36ef79f855273f
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-27 08:06:01 +0500

    HuggingFace ChatModel

---
+++ Models/ChatModels/apiChatHF.py
@@ -0,0 +1,15 @@
+## Run: python3 Models/ChatModels/apiChatHF.py
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llmHF = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llmHF)
+result = model.invoke("What's the capital of France?")
+
+print(result.content)
