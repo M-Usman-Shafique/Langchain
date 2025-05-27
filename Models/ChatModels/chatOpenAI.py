5f43703fad216f45e7d061b99a948ae3ae319c7f
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-27 07:20:02 +0500

    OpenAI ChatModel

---
+++ Models/ChatModels/chatOpenAI.py
@@ -0,0 +1,10 @@
+## Run python3 Models/ChatModels/chatOpenAI.py
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model= "gpt-4", temperature=0.7, max_completion_tokens=10)
+result = model.invoke("What's the capital of France?")
+
+print(result.content)
