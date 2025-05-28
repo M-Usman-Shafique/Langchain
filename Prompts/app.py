59dd648cf8bc2f2e77be9918022c94420d31a9ed
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-28 13:16:15 +0500

    Console chatbot with historical context

--- Prompts/app.py
+++ Prompts/app.py
@@ -6,9 +6,17 @@ load_dotenv()
 
 model = ChatOpenAI()
 
+chat_history = []
+
 while True:
     user_input = input('You: ')
+    chat_history.append(user_input)
+
     if user_input == 'exit':
         break
-    response = model.invoke(user_input)
+
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
     print("AI: ", response.content)
+
+print(chat_history)
