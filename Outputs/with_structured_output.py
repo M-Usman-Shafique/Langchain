7f3893e3aedc56ea25c14b5ff240debed21706b6
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-29 06:15:27 +0500

    Annotated structured output

--- Outputs/with_structured_output.py
+++ Outputs/with_structured_output.py
@@ -1,14 +1,14 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated
 
 load_dotenv()
 
 model = ChatOpenAI()
 
 class Review(TypedDict):
-    summary: str
-    sentiment: str
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[str, "Return sentiment of the review either negative, positive or neutral"]
 
 strctured_model = model.with_structured_output(Review)
 
