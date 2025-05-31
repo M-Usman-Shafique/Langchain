8d72ac72a43cabb3f22e5b32884cda34fb9add10
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-31 18:44:27 +0500

    StrOutputParser

---
+++ Outputs/Parsers/strOutputParser.py
@@ -0,0 +1,34 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llmHF = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+)
+
+model = ChatHuggingFace(llm=llmHF)
+
+template1 = PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=["topic"]
+)
+
+template2 = PromptTemplate(
+    template="Write a five line summary on the following text: /n{text}",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+
+# Parse the 1st response into string and pass it to the 2nd template and then parse the final response as well:
+chain = template1 | model | parser | template2 | model | parser
+
+response = chain.invoke({"topic": "black hole"})
+
+print(response)
---
+++ Outputs/Parsers/withoutParser.py
@@ -0,0 +1,32 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llmHF = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+)
+
+model = ChatHuggingFace(llm=llmHF)
+
+template1 = PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=["topic"]
+)
+
+template2 = PromptTemplate(
+    template="Write a five line summary on the following text: /n{text}",
+    input_variables=["text"]
+)
+
+prompt1 = template1.invoke({"topic": "blackhole"})
+response1 = model.invoke(prompt1)
+
+prompt2 = template2.invoke({"text": response1.content})
+response2 = model.invoke(prompt2)
+
+print(response2.content)
