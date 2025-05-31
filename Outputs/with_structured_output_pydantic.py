5a7cd246f8ef40867f19507d91f0cc860212b53d
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-31 11:06:44 +0500

    Structured output using pydantic

--- Outputs/with_structured_output_pydantic.py
+++ Outputs/with_structured_output_pydantic.py
@@ -1,17 +1,19 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict, Annotated, Optional, Literal
+from typing import Optional, Literal
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-class Review(TypedDict):
-    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
-    summary: Annotated[str, "A brief summary of the review"]
-    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
-    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
-    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
+class Review(BaseModel):
+    key_themes: list[str] = Field(description="Write down all the key themes discussed in the review in a list")
+    summary: str = Field(description="A brief summary of the review")
+    sentiment: Literal["pos", "neg"] = Field(description="Return sentiment of the review either negative, positive or neutral")
+    pros: Optional[list[str]] = Field(default=None, description="Write down all the pros inside a list")
+    cons: Optional[list[str]] = Field(default=None, description="Write down all the cons inside a list")
+    name: Optional[str] = Field(default=None, description="Write the name of the reviewer")
 
 strctured_model = model.with_structured_output(Review)
 
@@ -29,5 +31,9 @@ S-Pen support is unique and useful
                                  
 Review by John Doe
 """)
+# Direct access
+print(response["name"])
 
-print(response)
+# Convert to Pydantic model
+review = Review(**response)
+print(review.key_themes)
