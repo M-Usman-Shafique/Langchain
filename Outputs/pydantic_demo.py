1676818ba9b06e3d0d880f5f7b6267bd642be037
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-05-31 09:57:01 +0500

    Field constraints

--- Outputs/pydantic_demo.py
+++ Outputs/pydantic_demo.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Any, Optional
 
 class Student(BaseModel):
     age: int = 18
     name: Optional[str] = None
     email: EmailStr
+    cgpa: float = Field(gt=0, lt=5)
 
-new_student = {"age": 21, "email": "ali@test.com"}
+new_student = {"age": 21, "email": "ali@test.com", "cgpa": 4.99}
 
 student = Student(**new_student)
 
