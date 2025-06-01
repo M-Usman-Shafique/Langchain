594ff8e984ff8bfdadd72d94a6a8efa7ee722e2d
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-06-01 05:54:44 +0500

    Structured Output Parser

---
+++ Outputs/Parsers/structuredOutputParser.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llmHF = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+)
+
+model = ChatHuggingFace(llm=llmHF)
+
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give 3 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+response = chain.invoke({'topic':'blackhole'})
+
+print(response)
