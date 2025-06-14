f82babbeb7b209c2c62ffa70b8bc6c8234d1799b
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-06-14 08:55:10 +0500

    Runnables

---
+++ Runnables/runnable_branch.py
@@ -0,0 +1,36 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnablePassthrough, RunnableBranch
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+# report_gen_chain = RunnableSequence(prompt1, model, parser) (Old Syntax)
+report_gen_chain = prompt1 | model | parser # (LCEL)
+
+# If generated report is greater than 300 words, summarize it, otherwise print it as is:
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
+
+
